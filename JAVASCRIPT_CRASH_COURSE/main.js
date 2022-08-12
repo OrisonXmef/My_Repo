3388a1b709543ada79c5c3cc1fb0dac749514da9
OrisonXmef <orisonmustapha@gmail.com>
2022-08-12 11:46:36 +0000

    my first commit

---
+++ JAVASCRIPT_CRASH_COURSE/main.js
@@ -0,0 +1,22 @@
+// LOGICAL OPERATORS
+if (3==3 && 4==4){
+    document.writeln("The following condition is true.")
+}
+if (3==3 && 4==4){
+    document.writeln("The statement is false and will not be printed")  
+}
+const age = 21;
+if (age>18){
+    alert("You are really old")
+}
+else{alert("you are too old to register");
+}
+const x = 100;
+if (x<10){
+    document.getElementById("demo2").innerHTML="x is less than 10";
+}else if(x>10){
+    document.getElementById("demo2").innerHTML="x is greater than 10";
+}
+else{
+    document.getElementById("demo2").innerHTML="x is 10, it is neither greater nor less";
+}
